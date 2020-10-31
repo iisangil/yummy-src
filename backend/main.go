3aec274221edbf24335300c9e0cec25c06dda711
iisangil <iisangil@umich.edu>
2020-10-30 23:07:32 -0400

    changed mongo uri to python 3.3 or earlier due to srv issues

--- backend/main.go
+++ backend/main.go
@@ -29,6 +29,32 @@ type Post struct {
 	Body  string `json:"body"`
 }
 
+func main() {
+	// taken from https://docs.mongodb.com/drivers/go
+	uri := "mongodb://dbUser:dbUserPassword@cluster0-shard-00-00.htrlj.mongodb.net:27017,cluster0-shard-00-01.htrlj.mongodb.net:27017,cluster0-shard-00-02.htrlj.mongodb.net:27017/yummyDb?ssl=true&replicaSet=atlas-1lw2jg-shard-0&authSource=admin&retryWrites=true&w=majority"
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	var err error
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	// Ping the primary
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected and pinged.")
+
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/create", createGroup)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
 // insert a test data thing into the test collection
 func insertTest(title string, body string) {
 	post := Post{"hello", "goodbye"}
@@ -51,38 +77,12 @@ func getTest() {
 	fmt.Println("Found post with title ", post.Title)
 }
 
-func main() {
-	// taken from https://docs.mongodb.com/drivers/go
-	uri := "mongodb+srv://dbUser:dbUserPassword@cluster0.htrlj.mongodb.net/yummyDb?retryWrites=true&w=majority"
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	var err error
-	client, err = mongo.Connect(ctx, options.Client().ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			panic(err)
-		}
-	}()
-	// Ping the primary
-	if err := client.Ping(ctx, readpref.Primary()); err != nil {
-		panic(err)
-	}
-	fmt.Println("Successfully connected and pinged.")
-
-	// insertTest("hello", "goodbye")
-	// getTest()
-
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/create", createGroup)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
 func login(w http.ResponseWriter, r *http.Request) {
 	// login with phone number
-
+	if r.Method != "POST" {
+		http.Error(w, "This is the login page.", 405)
+		return
+	}
 }
 
 func createGroup(w http.ResponseWriter, r *http.Request) {
