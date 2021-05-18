b3988658d6d72739dc5cd88c028d5c607835ad66
iisangil <iisangil@umich.edu>
2021-05-17 22:39:31 -0400

    changed profile icon

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Avatar from '@material-ui/core/Avatar';
 
 import "./User.css";
@@ -6,10 +7,10 @@ import "./User.css";
 const User = ({ user }) => {
   return (
     <div className='user' >
-        <Avatar className='user__image' alt={user} src={'https://www.npmjs.com/npm-avatar/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXJVUkwiOiJodHRwczovL3MuZ3JhdmF0YXIuY29tL2F2YXRhci84ZTBlMGY2ODRiYWJjNjFiMTYwZjc5OGU0OWVjMjU4Zj9zaXplPTEwMCZkZWZhdWx0PXJldHJvIn0.-uAnfLlSw7sO91rZ8yvqNb4DYqQRlJTi6hmS1o5eskQ'} />
-        <div className='user__name' >
-            <h2>{user}</h2>
-        </div>
+      <AccountCircleIcon className='user__image' fontSize='large' />
+      <div className='user__name' >
+          <h2>{user}</h2>
+      </div>
     </div>
   );
 }
